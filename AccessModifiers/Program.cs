2aa88f324fa7b513ea96a3dfd518331fb9ab9b12
chichi-pixel <60243643+chichi-pixel@users.noreply.github.com>
2022-03-15 09:46:36 +0100

    AccessModifiers

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon;
+
+
+namespace AccessModifiers
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            var customer = new Customer();
+            Amazon.RateCalculator calculator = new RateCalculator();
+              
+            }
+        }
+       
+    
+}
---
+++ Amazon/RateCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace Amazon
+
+{
+    internal class RateCalculator
+    {
+        public int Calculate(Customer customer)
+        {
+            return 0;
+        }
+    }
+}
+
+
+//RateCalculator Class: in RateCalc simply calculator returns to zero!
